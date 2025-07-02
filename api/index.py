4e95f52acdc6d39af8758bb6e81b43f2dfd3e56b
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2025-07-02 22:05:14 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -39,9 +39,9 @@ async def create_group_from_json_request(json_data_str: str):
         request_data = json.loads(json_data_str)
         group_title = request_data.get("title")
         user_ids_to_add_string = request_data.get("user_ids", "")
-        user_ids_to_add = [number.strip() for number in user_ids_to_add_string.split(',')]
+        user_ids_to_add = [int(number.strip()) for number in user_ids_to_add_string.split(',')]
         phone_numbers_to_add_string = request_data.get("phone_numbers", "")
-        phone_numbers_to_add = [int(number.strip()) for number in phone_numbers_to_add_string.split(',')]
+        phone_numbers_to_add = [number.strip() for number in phone_numbers_to_add_string.split(',')]
 
         if not group_title:
             print("Error: Invalid JSON format. 'title' is required.")
