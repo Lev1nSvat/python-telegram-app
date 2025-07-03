3d42e72deb6ba6cb07c8c8715635e6d0d9e1e596
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2025-07-03 13:59:46 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -167,9 +167,10 @@ async def create_group_from_json_request(json_data_str: str):
                     print(item)
             users_names = users_names.strip()[:-1]
             users_not_added = set(user_ids_to_add).union(set(phone_numbers_to_add)).difference(set(users_to_add_to_group_call))
-            #if len(users_not_added) == 0:
+            if len(users_not_added) == 0:
             await client.send_message(new_group.id, f"\U0001F916 Чат создан автоматически, пригашённые пользователи: {users_names}. Все запрашиваемые пользователи были найдены.",disable_notification=True)
-            #else:
+            else:
+                client.send_message(new_group.id, users_not_added)
                 #await client.send_message(new_group.id,f"\U0001F916 Чат создан автоматически, пригашённые пользователи: {", ".join(users_to_add_to_group_call)}({users_names}).Пользователи {", ".join(users_not_added)} не найдены.Лог ошибок: {"\n".join(er_log)}", disable_notification=True)
             return {
                 "status": "success",
