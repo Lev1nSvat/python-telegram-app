b274a062623f5e4e9be0aedf71dcb0c4d5992578
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2025-07-17 11:50:14 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,8 +3,4 @@ from sanic.response import json
 
 app = Sanic("ChatCreator")
 
-@app.route("/")
-async def home(request):
-    return json({"message": "Hello from Sanic on Vercel!"})
-
 # No app.run() here!
