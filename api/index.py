16ce4d06cf491c8abe0515d2dcae773629e3e0d4
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2025-07-24 15:13:27 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -154,8 +154,6 @@ async def create_group_from_json_request(json_data_str: str):
                 "Authorization": "Bearer 3ed4629b-daf4-422d-9380-2369c0817e5f",
                 "Content-Type": "application/json"
             }
-            invite_link_obj = await Client.get_chat_invite_link(new_group.id)
-            print(f"invite link object: {invite_link_obj}")
             invite_url = await export_chat_invite_link(new_group.chat_id);
             payload = {
                 "context": {
