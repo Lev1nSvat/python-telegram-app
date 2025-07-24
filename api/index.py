d99f5bf1230396a41cfe18b1abd1cdf65d7f93f6
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2025-07-24 15:15:38 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -154,7 +154,7 @@ async def create_group_from_json_request(json_data_str: str):
                 "Authorization": "Bearer 3ed4629b-daf4-422d-9380-2369c0817e5f",
                 "Content-Type": "application/json"
             }
-            invite_url = await export_chat_invite_link(new_group.chat_id);
+            invite_url = await client.export_chat_invite_link(new_group.chat_id);
             payload = {
                 "context": {
                     "id_telegram_chata": invite_url
